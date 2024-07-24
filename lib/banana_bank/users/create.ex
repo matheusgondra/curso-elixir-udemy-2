c1f6c1888934d1a5b1f5b4c0de5530cddb9b2f0c
Matheus de Gondra <matheusgondra951@gmail.com>
2024-07-24 07:45:06 -0300

    busca cep do usuario no create

--- lib/banana_bank/users/create.ex
+++ lib/banana_bank/users/create.ex
@@ -1,10 +1,13 @@
 defmodule BananaBank.Users.Create do
+  alias BananaBank.ViaCep.Client, as: ViaCepClient
   alias BananaBank.Users.User
   alias BananaBank.Repo
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+  def call(%{"cep" => cep} = params) do
+    with {:ok, _result} <- ViaCepClient.call(cep) do
+      params
+      |> User.changeset()
+      |> Repo.insert()
+    end
   end
 end
