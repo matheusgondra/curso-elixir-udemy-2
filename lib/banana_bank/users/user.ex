493feaad7ab6a7a53e6e311d36298c6f1982af58
matheusgondra <matheusgondra951@gmail.com>
2024-07-23 22:29:17 -0300

    criando rota para atualização de user

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -6,6 +6,7 @@ defmodule BananaBank.Users.User do
   alias Ecto.Changeset
 
   @required_params [:name, :password, :email, :cep]
+  @required_params_update ~w(name email cep)a
 
   schema "users" do
     field :name, :string
@@ -17,15 +18,28 @@ defmodule BananaBank.Users.User do
     timestamps()
   end
 
-  def changeset(user \\ %__MODULE__{}, params) do
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> do_validations(@required_params)
+    |> unique_constraint(:email, name: :unique_email, message: "email already exists")
+    |> add_password_hash()
+  end
+
+  def changeset(user, params) do
     user
     |> cast(params, @required_params)
-    |> validate_required(@required_params)
+    |> do_validations(@required_params_update)
+    |> unique_constraint(:email, name: :unique_email)
+    |> add_password_hash()
+  end
+
+  defp do_validations(changeset, fields) do
+    changeset
+    |> validate_required(fields)
     |> validate_length(:name, min: 3)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:cep, is: 8)
-    |> unique_constraint(:email, name: :unique_email)
-    |> add_password_hash()
   end
 
   defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
