2434c625876cd03c897645c54b891f85d75b2f10
matheusgondra <matheusgondra951@gmail.com>
2024-07-24 23:59:22 -0300

    criando token de autenticação

--- lib/banana_bank/users.ex
+++ lib/banana_bank/users.ex
@@ -5,7 +5,7 @@ defmodule BananaBank.Users do
   alias BananaBank.Users.Create
 
   defdelegate create(params), to: Create, as: :call
-  defdelegate get(id), to: Get, as: :call
+  defdelegate get(id_or_email), to: Get, as: :call
   defdelegate update(params), to: Update, as: :call
   defdelegate delete(id), to: Delete, as: :call
 end
--- lib/banana_bank/users/get.ex
+++ lib/banana_bank/users/get.ex
@@ -2,10 +2,18 @@ defmodule BananaBank.Users.Get do
   alias BananaBank.Repo
   alias BananaBank.Users.User
 
+  def call(email) when is_bitstring(email) do
+    case Repo.get_by(User, email: email) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
+  end
+
   def call(id) do
     case Repo.get(User, id) do
       nil -> {:error, :not_found}
       user -> {:ok, user}
     end
   end
+
 end
